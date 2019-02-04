6c376c0646c0919a807c933b68a817f051e07a06
Ievgen <baranoffei@gmail.com>
2019-02-04 18:53:23 +0200

    Add async await to CRUD

--- lib/controllers/crmController.ts
+++ lib/controllers/crmController.ts
@@ -4,55 +4,85 @@ import { Request, Response } from "express";
 
 const Contact = mongoose.model("Contact", ContactSchema);
 export class ContactController {
-  public addNewContact(req: Request, res: Response) {
-    let newContact = new Contact(req.body);
-
-    newContact.save((err, contact) => {
-      if (err) {
-        res.send(err);
+  public async addNewContact(req: Request, res: Response): Promise<any> {
+    if (req.body) {
+      try {
+        const newContact = new Contact(req.body);
+        newContact.save();
+        res.status(201).send({ response: "Contact was create" });
+      } catch (err) {
+        return res.status(500).send(new Error("Something was wrong"));
       }
-      res.json(contact);
-    });
+    } else {
+      return res.status(404).send(new Error("Bad request"));
+    }
   }
 
-  public getContacts(req: Request, res: Response) {
-    Contact.find({}, (err, contact) => {
-      if (err) {
-        res.send(err);
+  public async getContacts(req: Request, res: Response): Promise<any> {
+    try {
+      const contacts = await Contact.find({});
+      if (!contacts) {
+        return res.status(404).send(new Error("CONTACT LIST not found"));
+      } else {
+        res.send(contacts);
       }
-      res.json(contact);
-    });
+    } catch (err) {
+      return res.status(500).send(new Error("Something was wrong"));
+    }
   }
 
-  public getContactWithID(req: Request, res: Response) {
-    Contact.findById(req.params.contactId, (err, contact) => {
-      if (err) {
-        res.send(err);
-      }
-      res.json(contact);
-    });
-  }
-
-  public updateContact(req: Request, res: Response) {
-    Contact.findOneAndUpdate(
-      { _id: req.params.contactId },
-      req.body,
-      { new: true },
-      (err, contact) => {
-        if (err) {
-          res.send(err);
+  public async getContactWithID(req: Request, res: Response): Promise<any> {
+    if (req.params) {
+      try {
+        const itemContact = await Contact.findById(req.params.contactId);
+        if (!itemContact) {
+          return res.status(404).send(new Error("CONTACT LIST not found"));
+        } else {
+          res.json(itemContact);
         }
-        res.json(contact);
+      } catch (err) {
+        return res.status(500).send(new Error("Something was wrong"));
       }
-    );
+    } else {
+      return res.status(404).send(new Error("Bad request"));
+    }
   }
 
-  public deleteContact(req: Request, res: Response) {
-    Contact.remove({ _id: req.params.contactId }, (err, contact) => {
-      if (err) {
-        res.send(err);
+  public async updateContact(req: Request, res: Response): Promise<any> {
+    if (req.body) {
+      try {
+        const updatedContact = await Contact.findOneAndUpdate(
+          {
+            _id: req.params.contactId
+          },
+          req.body,
+          { new: true }
+        );
+        if (!updatedContact) {
+          return res.status(404).send(new Error("Bad request"));
+        } else {
+          res.status(200).send(updatedContact);
+        }
+      } catch (err) {
+        return res.status(500).send(new Error("Something was wrong"));
       }
-      res.json({ message: "Successfully deleted contact!" });
-    });
+    } else {
+      return res.status(404).send(new Error("Bad request"));
+    }
+  }
+
+  public async deleteContact(req: Request, res: Response): Promise<any> {
+    try {
+      let itemUser = await Contact.remove({
+        _id: req.params.contactId
+      });
+      if (!itemUser) {
+        return res.status(404).send(new Error("Contact not found"));
+      } else {
+        res.status(204).send("Contact was successfulle deleted");
+      }
+    } catch (err) {
+      return res.status(500).send(new Error("Something was wrong"));
+    }
   }
 }
